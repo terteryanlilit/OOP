1a6d5a41078bef140de8bd644165f45866da0413
Lilit <112952247+terteryanlilit@users.noreply.github.com>
2023-10-03 20:46:07 +0400

    Create CommandController.hpp

---
+++ CommandController.hpp
@@ -0,0 +1,20 @@
+#ifndef COMMAND_CONTROLLER_COMMAND_CONTROLLER
+#define COMMAND_CONTROLLER_COMMAND_CONTROLLER
+
+#include "../Parser/Parser.hpp"
+
+#include <istream>
+
+class CommandController {
+public:
+    CommandController(std::istream&);
+    void exec();
+
+private:
+    void run();
+
+private:
+    Parser parser_;
+};
+
+#endif // COMMAND_CONTROLLER_COMMAND_CONTROLLER
