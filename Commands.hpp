d49083a7da26b3b1a155c4e5ea6015c0d4d6e255
Lilit <112952247+terteryanlilit@users.noreply.github.com>
2023-10-03 20:41:28 +0400

    Create Commands.hpp

---
+++ Commands.hpp
@@ -0,0 +1,57 @@
+#ifndef COMMAND_CONTROLLER_COMMAND
+#define COMMAND_CONTROLLER_COMMAND
+
+#include "Command.hpp"
+
+class Add : public Command {
+public:
+    Add();
+    virtual void addOperand(std::string option, double operand) override;
+    virtual void execute() override;
+    virtual Add* create() override;
+
+private:
+    void registerOptions();
+};
+
+class Sub : public Command {
+public:
+    Sub();
+    virtual void addOperand(std::string option, double operand) override;
+    virtual void execute() override;
+    virtual Sub* create() override;
+
+private:
+    void registerOptions();
+};
+
+class Mul : public Command {
+public:
+    Mul();
+    virtual void addOperand(std::string option, double operand) override;
+    virtual void execute() override;
+    virtual Mul* create() override;
+
+private:
+    void registerOptions();
+};
+
+class Div : public Command {
+public:
+    Div();
+    virtual void addOperand(std::string option, double operand) override;
+    virtual void execute() override;
+    virtual Div* create() override;
+
+private:
+    void registerOptions();
+};
+
+class Quit : public Command {
+public:
+    virtual void addOperand(std::string option, double operand) override;
+    virtual void execute() override;
+    virtual Quit* create() override;
+};
+
+#endif // COMMAND_CONTROLLER_COMMAND
