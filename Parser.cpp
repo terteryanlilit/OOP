1a3d5c974c8c4d489f24088fd0b2a574c82cca00
Lilit <112952247+terteryanlilit@users.noreply.github.com>
2023-10-03 21:07:50 +0400

    Create Parser.cpp

---
+++ Parser.cpp
@@ -0,0 +1,36 @@
+#include "Parser.hpp"
+
+#include <stdexcept> // std::runtime_error
+#include <string>
+#include <utility> // std::move
+
+Parser::Parser(std::istream& input)
+: input_{input}
+{
+}
+
+std::stringstream Parser::inputCommand() {
+    std::string command;
+    getline(input_, command);
+    return std::stringstream(command);
+}
+
+CommandPtr Parser::parseCommand() {
+
+    std::stringstream stream = inputCommand();
+
+    std::string token;
+    double op;                 
+
+    stream >> token;
+
+    CommandPtr commandPtr = commandRegistry_.findCommand(token);
+    if(!commandPtr)
+        throw std::runtime_error("invalid command!");
+
+    while(stream >> token) {
+            stream >> op;
+            commandPtr->addOperand(token, op);
+    }
+    return commandPtr;
+}
