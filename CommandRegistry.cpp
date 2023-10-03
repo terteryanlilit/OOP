387ac79e598deef13d4ff650075ce038f48f59a0
Lilit <112952247+terteryanlilit@users.noreply.github.com>
2023-10-03 20:42:53 +0400

    Create CommandRegistry.cpp

---
+++ CommandRegistry.cpp
@@ -0,0 +1,21 @@
+#include "CommandRegistry.hpp"
+#include "Commands.hpp"
+
+CommandRegistry::CommandRegistry() {
+    registerCommands();
+}
+
+void CommandRegistry::registerCommands() {
+    registry_["add"] = CommandPtr(new Add);
+    registry_["sub"] = CommandPtr(new Sub);
+    registry_["mul"] = CommandPtr(new Mul);
+    registry_["div"] = CommandPtr(new Div);
+    registry_["quit"] = CommandPtr(new Quit);
+}
+
+auto CommandRegistry::findCommand(std::string commandName) -> CommandPtr {
+    auto iter = registry_.find(commandName);
+    if(iter == registry_.end())
+        return nullptr;
+    return CommandPtr( iter->second->create() );
+}
